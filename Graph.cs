2d91281a61853c9962d6069b20d305b91045eb0f
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2022-09-07 18:12:49 -0400

    added kruskal's algorithm

--- Graph.cs
+++ Graph.cs
@@ -44,7 +44,72 @@ namespace GraphsClassProjectTakeTwo
 
         public List<Edge> Kruskal()
         {
-            return new List<Edge>();
+            List<Edge> shortestPath = new List<Edge>;
+            List<Edge> orderedEdges = this.edges.Sort((x, y) => x.Weight - y.Weight);
+
+            List<List<Vertex>> visited = new List<List<Vertex>>();
+
+            while (shortestPath.Count < this.vertices.Count - 1)
+            {
+                Edge shortest = orderedEdges.ElementAt(0);
+                int foundSoureWhere = -1;
+                int foundDestinationWhere = -1;
+
+                //check if will create cycle
+                foreach (List<Vertex> connectedVertices in visited)
+                {
+                    foreach (Vertex currVertex in connectedVertices)
+                    {
+                        if (currVertex == shortest.source)
+                        {
+                            foundSourceWhere = visited.IndexOf(connectedVertices);
+                        }
+                        else if (currVertex == shortest.Destination)
+                        {
+                            foundDestinationWhere = visited.IndexOf(connectedVertices);
+                        }
+                    }
+                }
+
+                if (foundSourceWhere == -1)
+                {
+                    if (foundDestinationWhere == -1)
+                    {
+                        List<Vertex> unconnected = new List<Vertex>();
+                        unconnected.Add(shortest.source);
+                        unconnected.Add(shortest.Destination);
+                        visited.Add(unconnected);
+                        shortestPath.Add(shortest);
+                    }
+                    else
+                    {
+                        visited[foundDestinationWhere].Add(shortest.source);
+                        shortestPath.Add(shortest);
+                    }
+                }
+                else
+                {
+                    if (foundDestinationWhere == -1)
+                    {
+                        visited[foundSourceWhere].Add(shortest.Destination);
+                        shortestPath.Add(shortest);
+                    }
+                    else if (foundDestinationWhere != foundSourceWhere)
+                    {
+                        foreach (Vertex movingVertex in visited[foundDestinationWhere])
+                        {
+                            visited[foundSourceWhere].Add(movingVertex);
+                        }
+
+                        visited.RemoveAt(foundDestinationWhere);
+                        shortestPath.Add(shortest);
+                    }
+                }
+
+                orderedEdges.RemoveAt(0);
+            }
+
+            return shortestPath;
 
         }
 
