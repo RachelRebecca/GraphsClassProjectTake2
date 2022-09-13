d2b1e1fa04589827061ff05dd2df164902e5a044
RachelRebecca <rachelrebecca67@gmail.com>
2022-09-13 19:19:47 -0400

    take two

--- Graph.cs
+++ Graph.cs
@@ -1,39 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace GraphsClassProjectTakeTwo
 {
     internal class Graph
     {
         // fields
-        private String name { get; set; }
+        private String Name { get; set; }
 
         private bool IsWeighted { get; set; }
 
         private bool IsDirected { get; set; }
 
-        private List<Vertex> vertices { get; set; }
+        private List<Vertex> Vertices { get; set; }
 
-        private List<Edge> edges { get; set; } // TODO: only edge list?
+        private List<Edge> Edges { get; set; } // TODO: only edge list?
 
         // constructor
         public Graph(String name, bool weighted, bool directed)
         {
-            this.name = name;
-            this.weighted = weighted;
-            this.directed = directed;
-            this.vertices = new List<Vertex>();
-            this.edges = new List<Edge>();
+            this.Name = name;
+            this.IsWeighted = weighted;
+            this.IsDirected = directed;
+            this.Vertices = new List<Vertex>();
+            this.Edges = new List<Edge>();
         }
 
 
         // methods
-        public bool loadGraph()
+        public bool LoadGraph(SqlConnection sqlCon, String name) // TODO: convert to GraphType
         {
-            return false;
+            bool retVal = true;
+            try
+            {
+                SqlCommand getEdgesForGraph = new SqlCommand("spGetEdges", sqlCon);
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@GraphName";
+                sqlParameter.Value = name;
+                getEdgesForGraph.Parameters.Add(sqlParameter);
+
+                getEdgesForGraph.CommandType = CommandType.StoredProcedure;
+                getEdgesForGraph.ExecuteNonQuery();
+                SqlDataAdapter da2 = new SqlDataAdapter(getEdgesForGraph);
+                DataSet dataSet = new DataSet();
+                da2.Fill(dataSet, "Edges");
+                // edge table: initialNode, terminalNode, weight (should be 1)
+
+                var nrEdges = dataSet.Tables["Edges"].Rows.Count;
+                for (int row = 0; row < nrEdges; ++row)
+                {
+                    // check initial node
+                    String initialNode = (String)dataSet.Tables["Edges"].Rows[row].ItemArray[0];
+                    String terminalNode = (String)dataSet.Tables["Edges"].Rows[row].ItemArray[1];
+
+                    int initialIndex = Vertices.FindIndex(item => initialNode.Equals(item.Name));
+                    int terminalIndex = Vertices.FindIndex(item => terminalNode.Equals(item.Name));
+
+                    Vertex initial = initialIndex < 0 ? new Vertex(initialNode)
+                                                    : Vertices[initialIndex];
+                    Vertex terminal = terminalIndex < 0 ? new Vertex(terminalNode)
+                                                    : Vertices[terminalIndex];
+
+                    if (initialIndex < 0 && terminalIndex < 0)
+                    {
+                        // neither exist - create both, add edge between them with weight = 1
+                        Vertices.Add(initial);
+                        Vertices.Add(terminal);
+                    }
+                    else if (initialIndex < 0 && terminalIndex > -1)
+                    {
+                        // initial doesn't exist, create and add edge between it and terminal with weight = 1
+                        Vertices.Add(initial);
+                    }
+                    else if (initialIndex > -1 && terminalIndex < 0)
+                    {
+                        // terminal doesn't exist, create and add edge between initial and it with weight = 1
+                        Vertices.Add(terminal);
+                    }
+                    // if they both already exist, no need to add anything
+                    initial.AddEdge(terminal, 1);
+                    terminal.AddEdge(initial, 1);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException());
+                Console.WriteLine(e.StackTrace);
+                retVal = false;
+            }
+
+            return retVal;
         }
 
         // todo: add an out parameter to return the sum of the weights of the total path
@@ -45,11 +108,11 @@ namespace GraphsClassProjectTakeTwo
         public List<Edge> Kruskal()
         {
             List<Edge> shortestPath = new List<Edge>;
-            List<Edge> orderedEdges = this.edges.Sort((x, y) => x.Weight - y.Weight);
+            List<Edge> orderedEdges = this.Edges.Sort((x, y) => x.Weight - y.Weight);
 
             List<List<Vertex>> visited = new List<List<Vertex>>();
 
-            while (shortestPath.Count < this.vertices.Count - 1)
+            while (shortestPath.Count < this.Vertices.Count - 1)
             {
                 Edge shortest = orderedEdges.ElementAt(0);
                 int foundSoureWhere = -1;
