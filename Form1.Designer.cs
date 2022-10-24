1541664c0a84975f07e54520dc2f3acd0d3457e4
RachelRebecca <rachelrebecca67@gmail.com>
2022-10-24 14:14:08 -0400

    added remove edge, remove node, add edge buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.Dijkstra = new System.Windows.Forms.Button();
             this.Kruskal = new System.Windows.Forms.Button();
             this.tableEdgesWeights = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.tableEdgesWeights)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,13 +54,13 @@
             this.panelGraph.BackColor = System.Drawing.Color.Gray;
             this.panelGraph.Location = new System.Drawing.Point(12, 12);
             this.panelGraph.Name = "panelGraph";
-            this.panelGraph.Size = new System.Drawing.Size(600, 550);
+            this.panelGraph.Size = new System.Drawing.Size(600, 535);
             this.panelGraph.TabIndex = 0;
             // 
             // Topological
             // 
             this.Topological.Enabled = false;
-            this.Topological.Location = new System.Drawing.Point(343, 574);
+            this.Topological.Location = new System.Drawing.Point(343, 553);
             this.Topological.Name = "Topological";
             this.Topological.Size = new System.Drawing.Size(104, 35);
             this.Topological.TabIndex = 1;
@@ -68,7 +71,7 @@
             // Prim
             // 
             this.Prim.Enabled = false;
-            this.Prim.Location = new System.Drawing.Point(495, 574);
+            this.Prim.Location = new System.Drawing.Point(497, 553);
             this.Prim.Name = "Prim";
             this.Prim.Size = new System.Drawing.Size(104, 35);
             this.Prim.TabIndex = 2;
@@ -79,7 +82,7 @@
             // Dijkstra
             // 
             this.Dijkstra.Enabled = false;
-            this.Dijkstra.Location = new System.Drawing.Point(23, 574);
+            this.Dijkstra.Location = new System.Drawing.Point(22, 553);
             this.Dijkstra.Name = "Dijkstra";
             this.Dijkstra.Size = new System.Drawing.Size(104, 35);
             this.Dijkstra.TabIndex = 1;
@@ -90,7 +93,7 @@
             // Kruskal
             // 
             this.Kruskal.Enabled = false;
-            this.Kruskal.Location = new System.Drawing.Point(188, 574);
+            this.Kruskal.Location = new System.Drawing.Point(188, 553);
             this.Kruskal.Name = "Kruskal";
             this.Kruskal.Size = new System.Drawing.Size(104, 35);
             this.Kruskal.TabIndex = 0;
@@ -113,6 +116,33 @@
             this.tableEdgesWeights.TabIndex = 3;
             this.tableEdgesWeights.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TableWeights_CellClick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 594);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Remove Edge";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(269, 594);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Remove Node";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(437, 594);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Add Edge";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // GraphProject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -120,6 +150,9 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ClientSize = new System.Drawing.Size(837, 621);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tableEdgesWeights);
             this.Controls.Add(this.panelGraph);
             this.Controls.Add(this.panelGraphButtons);
@@ -144,6 +177,9 @@
         private System.Windows.Forms.Button Dijkstra;
         private System.Windows.Forms.Button Kruskal;
         private System.Windows.Forms.DataGridView tableEdgesWeights;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
