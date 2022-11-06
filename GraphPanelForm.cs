86be472f78148f33aa246aa9a4e6580da0025014
RachelRebecca <rachelrebecca67@gmail.com>
2022-11-05 23:16:21 -0400

    more cleanup, commenting, etc.

--- GraphPanelForm.cs
+++ GraphPanelForm.cs
@@ -8,20 +8,24 @@ namespace GraphsClassProjectTakeTwo
 {
     public partial class GraphProject : Form
     {
+        /// <summary>
+        /// Shows the graph inside the panelGraph
+        /// </summary>
+        /// <param name="graph">The Graph to be displayed inside panelGraph</param>
         private void ShowGraph(Graph graph)
         {
-            CreateLabelType(graph);
-            CreateLabelsAndNodes(graph);
+            CreateLabelGraphType(graph);
+            CreateLabelNodes(graph);
+            CreateGraphicsNodes(graph);
             CreateLinesBetweenNodes(graph);
         }
 
-        private void CreateLabelType(Graph graph)
+        /// <summary>
+        /// Adds a label containing the Graph Type to the top left corner of panelGraph
+        /// </summary>
+        /// <param name="graph">The current graph being displayed</param>
+        private void CreateLabelGraphType(Graph graph)
         {
-            Label labelGraphType = new Label
-            {
-                Location = new Point(15, 20)
-            };
-
             string type = "";
             switch (graph.IsWeighted)
             {
@@ -41,34 +45,37 @@ namespace GraphsClassProjectTakeTwo
                     type += "Graph";
                     break;
             }
-
-            labelGraphType.Text = type;
-            labelGraphType.Size = new Size(150, 15);
+            Label labelGraphType = new Label
+            {
+                Location = new Point(15, 20),
+                Text = type,
+                Size = new Size(150, 15)
+            };
             labelGraphType.Refresh();
             panelGraph.Controls.Add(labelGraphType);
         }
 
-        private void CreateLabelsAndNodes(Graph graph)
+        /// <summary>
+        /// Create the labels near the nodes for the given Graph
+        /// </summary>
+        /// <param name="graph">The given Graph</param>
+        private void CreateLabelNodes(Graph graph)
         {
             List<Label> labelNodes = new List<Label>();
             for (int nodeNumber = 0; nodeNumber < graph.Vertices.Count; nodeNumber++)
             {
+                Point location = new Point((int)(graph.Vertices[nodeNumber].XCoord * panelGraph.Width), (int)(graph.Vertices[nodeNumber].YCoord * panelGraph.Height));
+
                 Label label = new Label
                 {
                     Text = graph.Vertices[nodeNumber].Name,
-                    TextAlign = ContentAlignment.MiddleCenter
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Location = GetNewXAndY(location),
+                    Font = new Font("Arial", 8),
+                    Size = new Size(20, 15),
+                    ForeColor = Color.White,
+                    Visible = true
                 };
-
-                Graphics graphics = panelGraph.CreateGraphics();
-                Pen pen = new Pen(Color.Black);
-                Point location = new Point((int)(graph.Vertices[nodeNumber].XCoord * panelGraph.Width), (int)(graph.Vertices[nodeNumber].YCoord * panelGraph.Height));
-                graphics.DrawEllipse(pen, location.X - 5, location.Y - 5, 10, 10);
-
-                label.Location = GetNewXAndY(location);
-                label.Font = new Font("Arial", 8);
-                label.Size = new Size(20, 15);
-                label.ForeColor = Color.White;
-                label.Visible = true;
                 label.Click += new EventHandler(Label_Click); ;
                 label.SendToBack();
                 label.Refresh();
@@ -83,6 +90,28 @@ namespace GraphsClassProjectTakeTwo
             }
         }
 
+        /// <summary>
+        /// Create the nodes for the given Graph using Graphics
+        /// </summary>
+        /// <param name="graph">The given Graph</param>
+        private void CreateGraphicsNodes(Graph graph)
+        {
+            for (int nodeNumber = 0; nodeNumber < graph.Vertices.Count; nodeNumber++)
+            {
+                Graphics graphics = panelGraph.CreateGraphics();
+                Pen pen = new Pen(Color.Black);
+                Point location = new Point((int)(graph.Vertices[nodeNumber].XCoord * panelGraph.Width), (int)(graph.Vertices[nodeNumber].YCoord * panelGraph.Height));
+                graphics.DrawEllipse(pen, location.X - 5, location.Y - 5, 10, 10);
+            }
+        }
+
+        /// <summary>
+        /// Get the labelNode's X and Y coordinates 
+        /// close enough to the graphics node that it is clear they are related,
+        /// but far enough that it is not sitting on top of the Graphics node itself
+        /// </summary>
+        /// <param name="location">The location of the Graphics node</param>
+        /// <returns></returns>
         private Point GetNewXAndY(Point location)
         {
             int xCoord;
@@ -93,12 +122,16 @@ namespace GraphsClassProjectTakeTwo
             else
                 xCoord = Math.Max(location.X - 15, 0);
             if (location.Y >= 200)
-                yCoord = Math.Min(location.Y + 15, panelGraph.Height);
+                yCoord = Math.Min(location.Y + 10, panelGraph.Height);
             else
                 yCoord = Math.Max(location.Y - 15, 0);
             return new Point(xCoord, yCoord);
         }
 
+        /// <summary>
+        /// Create the lines between all edges using Graphics for a given Graph using its Edges List
+        /// </summary>
+        /// <param name="graph">The given Graph</param>
         private void CreateLinesBetweenNodes(Graph graph)
         {
             SetUpGraphicsAndPen(graph.IsDirected, out Graphics graphics, out Pen pen, Color.Black);
@@ -113,22 +146,17 @@ namespace GraphsClassProjectTakeTwo
             }
         }
 
-        private void ResetPanel()
-        {
-            panelGraph.Controls.Clear();
-
-            panelGraph.Refresh();
-
-            panelGraph.BackColor = Color.Gray;
-        }
-
-        private void DrawRedLines(List<Edge> input)
+        /// <summary>
+        /// Given a list of edges, highlight them in Red
+        /// </summary>
+        /// <param name="edges">The list of edges</param>
+        private void DrawRedLines(List<Edge> edges)
         {
             SetUpGraphicsAndPen(CurrentGraph.IsDirected, out Graphics graphics, out Pen pen, Color.Red);
 
             List<string> edgeNames = new List<string>();
 
-            foreach (Edge edge in input)
+            foreach (Edge edge in edges)
             {
                 edgeNames.Add(edge.Start.Name + edge.End.Name);
                 Point initialLocation = new Point((int)(edge.Start.XCoord * panelGraph.Width), (int)(edge.Start.YCoord * panelGraph.Height));
@@ -139,7 +167,12 @@ namespace GraphsClassProjectTakeTwo
             HighlightSelectedEdgesInTable(edgeNames);
         }
 
-        private void DrawRedLines(List<Vertex> input, int sleepTime)
+        /// <summary>
+        /// Given a list of vertices, highlights them in Red one by one
+        /// </summary>
+        /// <param name="vertices">The list of vertices</param>
+        /// <param name="sleepTime">The length of time for the animation (in ms)</param>
+        private void DrawRedLines(List<Vertex> vertices, int sleepTime)
         {
             SetUpGraphicsAndPen(CurrentGraph.IsDirected, out Graphics graphics, out Pen pen, Color.Red);
 
@@ -147,12 +180,15 @@ namespace GraphsClassProjectTakeTwo
 
             List<string> edgeNames = new List<string>();
 
-            for (int i = 0; i < input.Count - 1; i++)
+            // go from start to second to last
+            for (int i = 0; i < vertices.Count - 1; i++)
             {
-                startingVertex = input[i];
-                endingVertex = input[i + 1];
+                // start = vertices at current index, end = the next vertex in vertices
+                startingVertex = vertices[i];
+                endingVertex = vertices[i + 1];
 
                 edgeNames.Add(startingVertex.Name + endingVertex.Name);
+
                 Point initialLocation = new Point((int)(startingVertex.XCoord * panelGraph.Width), (int)(startingVertex.YCoord * panelGraph.Height));
                 Point terminalLocation = new Point((int)(endingVertex.XCoord * panelGraph.Width), (int)(endingVertex.YCoord * panelGraph.Height));
                 graphics.DrawLine(pen, initialLocation, terminalLocation);
@@ -163,6 +199,13 @@ namespace GraphsClassProjectTakeTwo
             HighlightSelectedEdgesInTable(edgeNames);
         }
 
+        /// <summary>
+        /// Sets up the Graphics and Pen given a pen color
+        /// </summary>
+        /// <param name="useArrowCap">If using arrow caps (if IsDirected) or not</param>
+        /// <param name="graphics">out parameter for the Graphics</param>
+        /// <param name="pen">out parameter for the Pen</param>
+        /// <param name="penColor">The color of the Pen</param>
         private void SetUpGraphicsAndPen(bool useArrowCap, out Graphics graphics, out Pen pen, Color penColor)
         {
             graphics = panelGraph.CreateGraphics();
@@ -172,6 +215,18 @@ namespace GraphsClassProjectTakeTwo
                 AdjustableArrowCap adjustableArrowCap = new AdjustableArrowCap(3, 3);
                 pen.CustomEndCap = adjustableArrowCap;
             }
+        }
+
+        /// <summary>
+        /// Deletes all controls and Graphics in the panel 
+        /// </summary>
+        private void ResetPanel()
+        {
+            panelGraph.Controls.Clear();
+
+            panelGraph.Refresh();
+
+            panelGraph.BackColor = Color.Gray;
         }
     }
 }
